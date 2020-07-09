fe944ed41797b1a080c8f05f33354bed02a0054f
Varesh Tapadia <varesh.tapadia@rabobank.nl>
2020-07-09 15:47:51 +0200

    Retrieved the JSON before passing it to createGraphQLSchema

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const { createGraphQLSchema } = require('openapi-to-graphql')
- 
+const request = require('request');
+
 async function main(oas) {
-    console.log("Hello")
-  // generate schema:
+  // retrieve schema:
+  request(oas, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      createServer(JSON.parse(body))
+    } else {
+      console.log("Request failed", error, response);
+    }
+  });
+  
+}
+
+async function createServer(oas) {
   const { schema, report } = await createGraphQLSchema(oas)
-  console.log("Hello2")
- 
-  // server schema:
+
   const app = express()
   app.use(
     '/graphql',
@@ -17,10 +26,11 @@ async function main(oas) {
       graphiql: true
     })
   )
-  app.listen(3001)
+  app.listen(3000)
+  console.log("Server started on port 3000")
 }
 
-const oas = ["http://localhost:3000/openapi.json"]
+const oas = "http://localhost:3001/openapi.json"
 
 main(oas).catch(e => {
     console.log("app failed", e)
